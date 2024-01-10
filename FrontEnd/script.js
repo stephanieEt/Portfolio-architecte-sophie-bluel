25d0d922c4a18f028fc46977967f8e7985eba0f7
stephanieEt <stetourneau@laposte.net>
2024-01-10 08:45:01 +0100

    filter works

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,5 @@
 const gallery = document.querySelector(".gallery");
+const filter = document.querySelector(".filter");
 let liste = [];
 
 /*mettre à jour l'affichage*/
@@ -13,16 +14,65 @@ init();
 
 /*Affichage des nouveaux projets dans le dom*/
 async function displayProjects() {
-  const tabElements = await init();
-  tabElements.map((element) => {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-    img.src = element.imageUrl;
-    figcaption.innerText = element.title;
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
+  const card = await init();
+  card.map((card) => {
+    createCard(card);
   });
 }
 displayProjects();
+
+function createCard(card) {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  img.src = card.imageUrl;
+  figcaption.innerText = card.title;
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  gallery.appendChild(figure);
+}
+
+/*Fonction pour récupérer les catégories*/
+
+async function getCategorys() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  return await response.json();
+}
+getCategorys();
+
+/*Affichage des catégories dans le dom*/
+async function createCategorysButtons() {
+  const category = await getCategorys();
+  category.map((element) => {
+    const btn = document.createElement("button");
+    btn.innerText = element.name;
+    btn.id = element.id;
+    filter.appendChild(btn);
+  });
+}
+createCategorysButtons();
+
+/* Fonction pour filtrer les projets par catégoie*/
+async function filterCategory() {
+  const project = await init();
+  console.log(project);
+  const buttons = document.querySelectorAll(".filter button");
+  console.log(buttons);
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      btnId = e.target.id;
+      gallery.innerHTML = "";
+      if (btnId !== "0") {
+        const differentsCategory = project.filter((card) => {
+          return card.categoryId == btnId;
+        });
+        differentsCategory.forEach((card) => {
+          createCard(card);
+        });
+      } else {
+        displayProjects();
+      }
+    });
+  });
+}
+filterCategory();
